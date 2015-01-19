4bc81eaad3de7224fa8e748e038f590e79a5b4e3
hmurillop <hector@Hectors-MacBook-Air.local>
2015-01-19 16:43:46 -0600

    Base para trabajar

---
+++ js/main.js
@@ -0,0 +1,56 @@
+(function() {
+
+
+
+	var app = angular.module('afiliadosPac', ['ui.bootstrap']);
+
+
+
+
+
+
+//   	app.controller('AfiliadosPacController', function($scope, $http){
+  	 
+//   	});
+
+  	app.controller('ConsultaCedulaController', function ($scope, $http) {
+
+  		$scope.url = '../php/consultaCedula.php';
+
+
+  		$scope.clean = function () {
+  			$scope.cedula = '';
+  		}
+
+  		$scope.consultar= function() {
+					// Create the http post request
+					// the data holds the keywords
+					// The request is a JSON request.
+					$http.post($scope.url, { "data" : $scope.cedula})
+			
+				  .success(function(data, status) {
+				  	$scope.status = status;
+				  	$scope.data = data;
+				  	$scope.result = data; // Show result from server in our <pre></pre> element
+				  })
+				  .error(function(data, status) {
+				  	$scope.data = data || "Request failed";
+				  	$scope.status = status;			
+				  });
+				};  			
+  		}
+
+  		
+	});
+
+  		
+
+
+
+
+
+
+
+
+
+})();
