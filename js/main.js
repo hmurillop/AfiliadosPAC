681a7f97b22ee7dcf099ebd974de9f52f9247342
hmurillop <hector@Hectors-MacBook-Air.local>
2015-01-29 18:19:22 -0600

    Actualizacion

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 (function() {
-
-
-
 	var app = angular.module('afiliadosPac', ['ui.bootstrap']);
-
-
-
-
-
 
 //   	app.controller('AfiliadosPacController', function($scope, $http){
   	 
@@ -15,45 +7,38 @@
 
   	app.controller('ConsultaCedulaController', function ($scope, $http) {
 
-      $http.get('php/consultaCedula.php')
-      
-      .success(function(data) {
-        // here the data from the api is assigned to a variable named users
-        console.log(data);
-        $scope.user = data;
-        console.log($scope.user);
-      });
+      $scope.url = 'php/consultaCedula.php';
 
-      
+      $scope.search = function() {
+         
+         // Create the http post request
+         // the data holds the cedula
+         // The request is a JSON request.
+
+
+         $http.post($scope.url, { "data" : $scope.cedula})
+         .
+         success(function(data, status) {
+            $scope.status = status;
+            $scope.data = data;
+            $scope.result = data; // Show result from server in our <pre></pre> element
+         })
+         .
+         error(function(data, status) {
+            $scope.data = data || "Request failed";
+            $scope.status = status;       
+         });
+      };   
+
+
+
+
+
 
 
   		$scope.clean = function () {
   			$scope.cedula = '';
   		}
-
-  		// $scope.consultar= function() {
-		
-
-
-
-  		// }
-
-
-      console.log($scope.user);
-
-
-
-  		
 	});
-
-  		
-
-
-
-
-
-
-
-
-
 })();
+
